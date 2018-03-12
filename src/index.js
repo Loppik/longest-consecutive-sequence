17a4c7693ceeeb080f274d3358840b5e37cc3566
Alex <shantyr_qq@mail.ru>
2018-03-12 19:31:45 +0300

    production

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  let count = 1, res = 1;
+  let count = 1, res = 1, last;
+  
   if (array.length == 0) {
-  res = 0;
+    res = 0;
   }
   for (let i = 0; i < array.length - 1; i++) {
-  if (array[i] + 1 == array[i+1]) {
-    count += 1;
-  } else {
-    if (count > res) {
-      res = count;
+    if (array[i] + 1 == array[i+1]) {
+      count += 1;
+    } else {
+      if (array[i] == array[i+1]) { continue; }
+      if (count > res) {
+        last = array[i];
+        res = count;
+      }
+      count = 1;
     }
-    count = 1;
-  }
   }
   return res;
 }
